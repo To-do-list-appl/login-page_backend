9dc4d1671604feb0dacdae76cb12156732516ffb
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-07-07 19:38:35 +0700

    l

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,25 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import googleConfig from 'eslint-config-google';
 
 export default [
-  googleConfig, 
   js.configs.recommended,
   {
     files: ['**/*.js'],
     languageOptions: {
       sourceType: 'commonjs',
-    },
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
     },
   },
 ];
