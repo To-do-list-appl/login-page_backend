36ca8375ba6360491219ad313f144038e1250c23
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-07-13 12:17:29 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const CORS = require('cors');
 const routes = require('./src/routes'); // Import your routes
 require('dotenv').config(); // Load environment variables from .env file
 
+const corsOptions = {
+  origin: 'http://localhost:5173/'
+};
+
+app.use(CORS(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 routes(app); // Initialize routes
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Server is running');
 });
 
 app.listen(port, () => {
